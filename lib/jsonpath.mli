c6992410c1c3e5ae069cc75236ae9d6706d168a0
Kevin Schoon <kevinschoon@gmail.com>
2020-09-07 13:28:58 -0500

    clean up comments, update opam dependencies

--- lib/jsonpath.mli
+++ lib/jsonpath.mli
@@ -1,27 +1,22 @@
 type t
-
-(*
-A JSONPath can be used to search for arbitrary data within a JSON structure.
-*)
+(** A JSONPath can be used to search for arbitrary data within a JSON structure. *)
 
 val to_string : t -> string
-
-(* convert a JSONPath into a pretty printed string *)
+(** convert a JSONPath into a pretty printed string *)
 
 val of_string : string -> t
-
-(* convert a string into a JSONPath *)
+(** convert a string into a JSONPath *)
 
 module Yojson : sig
   val select : t -> Yojson.Basic.t -> Yojson.Basic.t list
-  (* Apply the components of the path
+  (** Apply the components of the path
      to each JSON value in the list of values returned so far,
      starting from the root. *)
 end
 
 module Jsonm : sig
   val select : t -> Ezjsonm.value -> Ezjsonm.value list
-  (* Apply the components of the path
+  (** Apply the components of the path
      to each JSON value in the list of values returned so far,
      starting from the root. *)
 end
